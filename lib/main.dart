f5cff3729bc1f7f296222fc9e8f0ca52b9973c0a
taimatu7 <taimatu7@outlook.jp>
2023-06-01 11:48:06 +0900

    リリースビルドをできるようにする

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'TODOリスト',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Common.themeColor),
